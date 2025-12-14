3878149f91b65b48a695f974809f2c88a70efd74
WEBZaytsev <nikita@zaitsv.dev>
2025-12-14 07:30:23 +0300

    feat: add YooKassaSdk export to index for enhanced SDK functionality

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,4 +1,4 @@
 import type { ConnectorOpts, ProxyConfig } from './connector'
-import { clearYooKassaCache, YooKassa } from './sdk'
-export { YooKassa, clearYooKassaCache }
+import { clearYooKassaCache, YooKassa, YooKassaSdk } from './sdk'
+export { YooKassa, YooKassaSdk, clearYooKassaCache }
 export type { ConnectorOpts, ProxyConfig }
